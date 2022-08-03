11e38c67941806a0703896d815ee928fb7a1b24f
Jeroen van der Heijden <jeroen@cesbit.com>
2022-08-03 13:08:17 +0200

    Update siridb.py

--- lib/siridb/siridb.py
+++ lib/siridb/siridb.py
@@ -14,9 +14,8 @@ async def query_series_datapoint_count(siridb_client, series_name):
             f'select count() from "{series_name}"')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     else:
         counts = result.get(series_name, [])
         if counts:
@@ -31,9 +30,8 @@ async def query_time_unit(siridb_client):
             f'show time_precision')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     if result is None:
         return None
     return result["data"][0]["value"]
@@ -46,9 +44,8 @@ async def query_series_data(siridb_client, series_name, selector="*"):
             f'select {selector} from "{series_name}"')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
         pass
     return result
 
@@ -59,9 +56,8 @@ async def drop_series(siridb_client, series_name):
         result = await siridb_client.query(f'drop series {series_name}')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
 
 
@@ -71,9 +67,8 @@ async def insert_points(siridb_client, series_name, points):
         await siridb_client.insert({series_name: points})
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
 
 
@@ -84,9 +79,8 @@ async def query_group_expression_by_name(siridb_client, group_name):
             f'list groups where name == "{group_name}"')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     groups = result.get('groups')
     if groups is None or len(groups) < 1:
         return None
@@ -102,9 +96,8 @@ async def query_all_series_results(siridb_client, series_name, selector="*"):
             f'/enodo_{re.escape(series_name)}_.*?$/')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
 
 
@@ -120,9 +113,8 @@ async def query_series_forecasts(siridb_client, series_name, selector="*",
             f'/enodo_{re.escape(series_name)}_forecast_.*?$/{after}')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
 
 
@@ -134,9 +126,8 @@ async def query_series_anomalies(siridb_client, series_name, selector="*"):
             f'/enodo_{re.escape(series_name)}_anomalies_.*?$/')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
 
 
@@ -149,7 +140,6 @@ async def query_series_static_rules_hits(
             f'/enodo_{re.escape(series_name)}_static_rules_.*?$/')
     except (QueryError, InsertError, ServerError, PoolError,
             AuthenticationError, UserAuthError) as e:
-        logging.error("Connection problem with SiriDB server")
         msg = str(e) or type(e).__name__
-        logging.debug(f"Problem with SiriDB: {msg}")
+        logging.error(f"Problem with SiriDB: {msg}")
     return result
